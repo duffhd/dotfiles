8b356342cea8b0a0802c73e74ba6c432f90f059d
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-01-26 00:07:55 -0300

    scroll-margin to 8

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -27,13 +27,15 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type t
+      scroll-margin 8)
 
 
 ;; Here are some additional functions/macros that could help you configure Doom:
